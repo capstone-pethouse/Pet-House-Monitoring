fe79d2d5e4a0c2db602f1c76a1aa2b5335907a5a
yoojanghun <yoojanghun@naver.com>
2026-03-24 22:55:24 +0900

    #2 - Java, Spring Boot 환경설정
    Project: Gradle - Groovy
    Language: Java 21
    Spring Boot: 3.5.12
    Dependency: Spring Web, Spring Data JPA, Lombok, MySQL Driver, Spring Boot DevTools, Spring Security, Validation

---
+++ src/main/java/com/capstone/pethouse/PetHouseMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.pethouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHouseMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHouseMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/pethouse/PetHouseMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.pethouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHouseMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
