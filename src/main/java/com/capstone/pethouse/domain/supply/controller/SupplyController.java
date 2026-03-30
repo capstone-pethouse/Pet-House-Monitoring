92c8b20eec1e9875cd510cc0f09582a1d0bf394b
yoojanghun <yoojanghun@naver.com>
2026-03-30 23:09:11 +0900

    #16 - 기존의 dispenseFood를 recordSupplyLog로 변경
    원래 dispenseFood는 수동 급식 / 급수 제어 시 사용하려고 했었음.
    이를 수동 및 자동으로 급식 / 급수가 제공되거나 제공될 예정일 때, 로그를 남기는 controller로 변환

--- src/main/java/com/capstone/pethouse/domain/supply/controller/SupplyController.java
+++ src/main/java/com/capstone/pethouse/domain/supply/controller/SupplyController.java
@@ -1,8 +1,8 @@
 package com.capstone.pethouse.domain.supply.controller;
 
-import com.capstone.pethouse.domain.supply.dto.request.DispenseRequest;
+import com.capstone.pethouse.domain.supply.dto.request.SupplyLogRequest;
 import com.capstone.pethouse.domain.supply.dto.request.ScheduleRequest;
-import com.capstone.pethouse.domain.supply.dto.response.DispenseResponse;
+import com.capstone.pethouse.domain.supply.dto.response.SupplyLogResponse;
 import com.capstone.pethouse.domain.supply.dto.response.ScheduleResponse;
 import com.capstone.pethouse.domain.supply.service.SupplyService;
 import lombok.RequiredArgsConstructor;
@@ -15,21 +15,21 @@ public class SupplyController {
 
     private final SupplyService supplyService;
 
-    // 수동 급수 / 급식 제어
-    @PostMapping("/{id}/supplier/dispense")
-    public DispenseResponse dispenseFood(
-            @PathVariable Long id,                          // pet house의 아이디
-            @RequestBody DispenseRequest dispenseRequest
-    ) {
-        return supplyService.dispenseFood(id, dispenseRequest);
-    }
-
-    // 자동 급수 / 급식 제어
+    // 자동 급수 / 급식 스케줄러 등록
     @PostMapping("/{id}/supplier/schedules")
     public ScheduleResponse postSchedule(
             @PathVariable Long id,
             @RequestBody ScheduleRequest scheduleRequest
     ) {
         return supplyService.postSchedule(id, scheduleRequest);
+    }
+
+    // 제공된 급식 / 급수에 대한 supply_log 저장
+    @PostMapping("/{id}/supplier/record")
+    public SupplyLogResponse recordSupplyLog(
+            @PathVariable Long id,                          // pet house의 아이디
+            @RequestBody SupplyLogRequest supplyLogRequest
+    ) {
+        return supplyService.recordSupplyLog(id, supplyLogRequest);
     }
 }
