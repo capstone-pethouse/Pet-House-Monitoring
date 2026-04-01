787713a564f3ecf55c0450b3e4c72135117f48c5
yoojanghun <yoojanghun@naver.com>
2026-04-01 13:39:32 +0900

    #19 - 급식 / 급수 수정하는 controller 작성
    기존에 supply_log를 저장하는 recordSupplyLog는 수동 제공된 급식 / 급수에 대해서만 호출.
    스케줄러를 활용하여 자동 호출된 급식 / 급수는 MQTT 리스너를 통하여 IoT 기기로부터 신호를 받고,
    supply_log에 기록하도록 변경함. (자동 급식 / 급수는 POST 요청을 날릴 필요가 없음)
    추가적으로 가독성 좋게 id => houseId로 변경

--- src/main/java/com/capstone/pethouse/domain/supply/controller/SupplyController.java
+++ src/main/java/com/capstone/pethouse/domain/supply/controller/SupplyController.java
@@ -16,20 +16,31 @@ public class SupplyController {
     private final SupplyService supplyService;
 
     // 자동 급수 / 급식 스케줄러 등록
-    @PostMapping("/{id}/supplier/schedules")
+    @PostMapping("/{houseId}/supplier/schedules")
     public ScheduleResponse postSchedule(
-            @PathVariable Long id,
+            @PathVariable Long houseId,
             @RequestBody ScheduleRequest scheduleRequest
     ) {
-        return supplyService.postSchedule(id, scheduleRequest);
+        return supplyService.postSchedule(houseId, scheduleRequest);
     }
 
-    // 제공된 급식 / 급수에 대한 supply_log 저장
-    @PostMapping("/{id}/supplier/record")
+    // 자동 급수 / 급식 스케줄러 수정
+    @PutMapping("/{houseId}/supplier/schedules/{scheduleId}")
+    public ScheduleResponse updateSchedule(
+            @PathVariable Long houseId,
+            @PathVariable Long scheduleId,
+            @RequestBody ScheduleRequest scheduleRequest
+    ) {
+        return supplyService.updateSchedule(houseId, scheduleId, scheduleRequest);
+    }
+
+    // 제공된 급식 / 급수에 대한 supply_log 저장 (수동 제공시)
+    // 자동 급식 / 급수 제공 시엔 MQTT 리스너가 서비스 호출
+    @PostMapping("/{houseId}/supplier/record")
     public SupplyLogResponse recordSupplyLog(
-            @PathVariable Long id,                          // pet house의 아이디
+            @PathVariable Long houseId,                          // pet house의 아이디
             @RequestBody SupplyLogRequest supplyLogRequest
     ) {
-        return supplyService.recordSupplyLog(id, supplyLogRequest);
+        return supplyService.recordSupplyLog(houseId, supplyLogRequest);
     }
 }
