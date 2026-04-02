0a2e1bd0908232245b71859987a4cfc797d851c4
yoojanghun <yoojanghun@naver.com>
2026-04-02 23:13:19 +0900

    #28 - FAN의 스케줄러에 접근하는 repository

---
+++ src/main/java/com/capstone/pethouse/domain/fan/repository/FanScheduleRepository.java
@@ -0,0 +1,12 @@
+package com.capstone.pethouse.domain.fan.repository;
+
+import com.capstone.pethouse.domain.fan.entity.FanSchedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FanScheduleRepository extends JpaRepository<FanSchedule, Long> {
+    Page<FanSchedule> findByPetHouse_HouseId(Long houseId, Pageable pageable);
+}
