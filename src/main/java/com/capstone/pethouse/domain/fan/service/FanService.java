2e63fa95dee581e2fcf02e18c97c3de47924dc84
yoojanghun <yoojanghun@naver.com>
2026-04-05 12:43:08 +0900

    #27 - FanSchedule과 FanScheduleDetail을 저장하는 Service 로직
    하나의 FanScheduleRequest에는 여러 개의 FanScheduleDetailRequest가 있다.
    이를 FanSchedule 엔티티에서 정의한 addFanScheduleDetail 메서드를 호출해 각각 저장.
    FanSchedule 엔티티에선 CascadeType.ALL을 설정해 놓았기 때문에, 부모의 상태 변화는 자식에게도 전달됨.
    JPA는 부모 객체 안의 detail들의 리스트를 훑어본 후, DB에 저장되지 않은 detail들은 db에 저장함.

--- src/main/java/com/capstone/pethouse/domain/fan/service/FanService.java
+++ src/main/java/com/capstone/pethouse/domain/fan/service/FanService.java
@@ -1,5 +1,8 @@
 package com.capstone.pethouse.domain.fan.service;
 
+import com.capstone.pethouse.domain.device.entity.PetHouse;
+import com.capstone.pethouse.domain.device.repository.PetHouseRepository;
+import com.capstone.pethouse.domain.fan.dto.request.FanScheduleRequest;
 import com.capstone.pethouse.domain.fan.dto.response.FanScheduleResponse;
 import com.capstone.pethouse.domain.fan.dto.response.FanToggleResponse;
 import com.capstone.pethouse.domain.fan.entity.FanSchedule;
@@ -17,12 +20,31 @@ import org.springframework.transaction.annotation.Transactional;
 public class FanService {
 
     private final FanScheduleRepository fanScheduleRepository;
+    private final PetHouseRepository petHouseRepository;
 
     @Transactional(readOnly = true)
     public Page<FanScheduleResponse> getFanSchedules(Long houseId, Pageable pageable) {
         Page<FanSchedule> fanSchedulePage = fanScheduleRepository.findByPetHouse_HouseId(houseId, pageable);
 
         return fanSchedulePage.map(FanScheduleResponse::from);
+    }
+
+    public FanScheduleResponse postFanSchedule(Long houseId, FanScheduleRequest fanScheduleRequest) {
+        if (fanScheduleRequest.endTime().isBefore(fanScheduleRequest.startTime())) {
+            throw new IllegalArgumentException("시작 시간은 종료 시간보다 빨라야 합니다.");
+        }
+
+        if (fanScheduleRepository.existingOverlappingSchedule(houseId, fanScheduleRequest.startTime(), fanScheduleRequest.endTime())) {
+            throw new IllegalStateException("해당 시간대에 이미 겹치는 팬 스케줄이 존재합니다.");
+        }
+
+        PetHouse petHouse = petHouseRepository.getReferenceById(houseId);
+        FanSchedule fanSchedule = FanSchedule.of(petHouse, fanScheduleRequest.startTime(), fanScheduleRequest.endTime());
+
+        fanScheduleRequest.fanScheduleDetailRequestList()
+                .forEach(request -> fanSchedule.addFanScheduleDetail(request.temperature(), request.speed()));
+
+        return FanScheduleResponse.from(fanScheduleRepository.save(fanSchedule));
     }
 
     public FanToggleResponse toggleFanSchedule(Long houseId, Long scheduleId, boolean enabled) {
