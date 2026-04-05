e396125db269687123a1b3a034f373a13b5e6b4e
yoojanghun <yoojanghun@naver.com>
2026-04-05 12:24:08 +0900

    #27 - FanController에 Fan 스케줄러 등록하는 POST 메서드 작성

--- src/main/java/com/capstone/pethouse/domain/fan/controller/FanController.java
+++ src/main/java/com/capstone/pethouse/domain/fan/controller/FanController.java
@@ -1,9 +1,10 @@
 package com.capstone.pethouse.domain.fan.controller;
 
+import com.capstone.pethouse.domain.fan.dto.request.FanScheduleRequest;
 import com.capstone.pethouse.domain.fan.dto.response.FanScheduleResponse;
 import com.capstone.pethouse.domain.fan.dto.response.FanToggleResponse;
 import com.capstone.pethouse.domain.fan.service.FanService;
-import com.capstone.pethouse.domain.supply.dto.response.SupplyToggleResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,6 +26,15 @@ public class FanController {
             @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ) {
         return fanService.getFanSchedules(houseId, pageable);
+    }
+
+    // 자동 FAN 스케줄러 등록
+    @PostMapping("/{houseId}/fan/schedules")
+    public FanScheduleResponse postSupplySchedule(
+            @PathVariable Long houseId,
+            @Valid @RequestBody FanScheduleRequest fanScheduleRequest
+    ) {
+        return fanService.postFanSchedule(houseId, fanScheduleRequest);
     }
 
     // 자동 FAN 스케줄러 활성, 비활성 토글
